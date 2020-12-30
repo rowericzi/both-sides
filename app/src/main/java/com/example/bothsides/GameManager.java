bd4d281a4c9f57892804485f1a5a853674cad08a
Ryszard Jezierski <jezierski.ryszard@gmail.com>
2020-12-30 16:14:39 +0100

    Implemented result calculation

--- app/src/main/java/com/example/bothsides/GameManager.java
+++ app/src/main/java/com/example/bothsides/GameManager.java
@@ -18,12 +18,14 @@ import java.util.TimerTask;
 public class GameManager extends Thread {
 	private final static int ANIMATION_DURATION = 1500;
 	private final static int INITIAL_DELAY = 1000;
+	private final static int LATENCY = 200;
+	private static int INITIAL_METRONOME_DURATION = 0;
 	private final Context context;
 	private final RelativeLayout imgHolder;
 	private final ArrayList<ImageView> imgList = new ArrayList<>();
 	private final RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(128,128);
 	private final ArrayList<Integer> timestamps = new ArrayList<>();
-	private final ArrayList<Integer> userInputTimestamps = new ArrayList<>();
+	private final ArrayList<Long> userInputTimestamps = new ArrayList<>();
 	private Long startTime;
 	//these are global, so they can be accessed by cancelGame();
 	private final Timer endGameTimer = new Timer();
@@ -87,9 +89,10 @@ public class GameManager extends Thread {
 	}
 
 	private void InitTimestamps(double tempo, int metre, int repetitions, double[] rhythm, double patternLength) {
+		INITIAL_METRONOME_DURATION = (int) Math.round(60000*metre/tempo);
 		for (int i = 0; i < repetitions; i++){
 			for ( double note : rhythm ){
-				timestamps.add((int) Math.round(60000*(metre + note + patternLength * i )/tempo + INITIAL_DELAY - ANIMATION_DURATION));
+				timestamps.add((int) Math.round(60000*(note + patternLength * i )/tempo + INITIAL_DELAY +INITIAL_METRONOME_DURATION - ANIMATION_DURATION));
 			}
 		}
 	}
@@ -130,13 +133,59 @@ public class GameManager extends Thread {
 	}
 
 	public void processUserInput() {
-		userInputTimestamps.add((int) (System.nanoTime() - startTime)/1000000);
+		userInputTimestamps.add(System.nanoTime());
 	}
 
 	public void endGame() {
-		((Level) context).endGame(this, 2137.0);
 		metronomeTimer.cancel();
 		metronomeTimer.purge();
+		ArrayList<Integer> logTimestampsCorrected = new ArrayList<>();
+		ArrayList<Integer> results = new ArrayList<>();
+		double result;
+		for (Long timestamp : userInputTimestamps) {
+			int correctedTimestamp = Math.toIntExact((timestamp-startTime)/1000000 - ANIMATION_DURATION - LATENCY);
+			int closest = searchClosest(timestamps, correctedTimestamp);
+			results.add(closest - correctedTimestamp);
+			logTimestampsCorrected.add(correctedTimestamp);
+		}
+		Log.d("Timestamps", String.valueOf(timestamps));
+		Log.d("InputTimestampCorrected", String.valueOf(logTimestampsCorrected));
+		Log.d("Results", String.valueOf(results));
+		if (results.size() == timestamps.size()) {
+			result = calculateResult(results);
+		} else {
+			result = 2.0;
+		}
+		((Level) context).endGame(this, result);
+
+	}
+
+	private int searchClosest (ArrayList<Integer> arrayList, int key) {
+		int minimumValue = Integer.MAX_VALUE;
+		int minimumIndex = 0;
+		for (int i = 0; i < arrayList.size(); i++) {
+			if (Math.abs(arrayList.get(i) - key) < minimumValue) {
+				minimumValue = Math.abs(arrayList.get(i) - key);
+				minimumIndex = i;
+			}
+		}
+		return arrayList.get(minimumIndex);
+	}
+
+	private double calculateResult(ArrayList<Integer> arrayList) {
+		int sum = 0;
+		double variance = 0.0;
+		for (int elem : arrayList) {
+			sum += elem;
+		}
+		double mean = sum/(double)arrayList.size();
+		Log.d("mean", String.valueOf(mean));
+		for (int elem : arrayList) {
+			variance += Math.pow(elem - mean, 2);
+		}
+		variance = variance /arrayList.size();
+		Log.d("stdDev", String.valueOf(Math.sqrt(variance)));
+		return 10 - (Math.sqrt(variance) + Math.abs(mean))*8/500;
 	}
 
 	public void cancelGame() {
