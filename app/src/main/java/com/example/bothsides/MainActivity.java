fc489fe7f3ac9af81ebdc1d42b7f5ca69b2c2b55
Ryszard Jezierski <jezierski.ryszard@gmail.com>
2020-12-30 19:57:00 +0100

    Choosing rhythm patterns now works properly

--- app/src/main/java/com/example/bothsides/MainActivity.java
+++ app/src/main/java/com/example/bothsides/MainActivity.java
@@ -4,12 +4,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity{
 	public static final String EXTRA_TEMPO = "com.example.bothsides.TEMPO";
@@ -25,32 +27,47 @@ public class MainActivity extends AppCompatActivity{
 	public static final String EXTRA_RESULT_1 = "com.example.bothsides.RESULTS_1";
 	public static final String EXTRA_RESULT_2 = "com.example.bothsides.RESULTS_2";
 	public static final String EXTRA_HAS_RESULT_2 = "com.example.bothsides.HAS_RESULTS_2";
+
+	private static class RhythmPattern {
+		public String name;
+		public int image;
+		public double patternLength;
+		public double[] pattern;
+
+		RhythmPattern(String name, int image, double patternLength, double[] pattern) {
+			this.name = name;
+			this.image = image;
+			this.patternLength = patternLength;
+			this.pattern = pattern;
+		}
+	}
+	ArrayList<RhythmPattern> rhythmPatternList = new ArrayList<>();
+
 	Context context = this;
 	Spinner spinner1;
 	Spinner spinner2;
-	String[] listItems = {"item 1", "item 2 ", "item 3", "item 4", "item 2137"};
-	int[] listImages = {
-		R.drawable.papryka,
-		R.drawable.papryka,
-		R.drawable.papryka,
-		R.drawable.papryka,
-		R.drawable.papryka};
+	RhythmPattern chosenRhythmPattern1;
+	RhythmPattern chosenRhythmPattern2;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		initializeRhythmPatternList();
+		ImageArrayAdapter adapter = initializeImageArrayAdapter();
+
 		DisplayMetrics metrics = new DisplayMetrics();
 		getWindowManager().getDefaultDisplay().getMetrics(metrics);
 
-		ImageArrayAdapter adapter = new ImageArrayAdapter(this, listItems, listImages);
 		spinner1 = (Spinner) findViewById(R.id.spinner1);
 		spinner1.setDropDownWidth(metrics.widthPixels);
 		spinner1.setAdapter(adapter);
 		spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 			@Override
 			public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-				Toast.makeText(context, "spinner 1" + listItems[position], Toast.LENGTH_SHORT).show();
+				chosenRhythmPattern1 = rhythmPatternList.get(position);
+				Log.d("Spinner1", chosenRhythmPattern1.name);
 			}
 
 			@Override
@@ -64,7 +81,8 @@ public class MainActivity extends AppCompatActivity{
 		spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 			@Override
 			public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-				Toast.makeText(context, "spinner 2" + listItems[position], Toast.LENGTH_SHORT).show();
+				chosenRhythmPattern2 = rhythmPatternList.get(position);
+				Log.d("Spinner2", chosenRhythmPattern2.name);
 			}
 
 			@Override
@@ -79,23 +97,39 @@ public class MainActivity extends AppCompatActivity{
 		//using hard-coded values for now
 		intent.putExtra(EXTRA_TEMPO, 60.0);
 		intent.putExtra(EXTRA_METRE, 4);
-		intent.putExtra(EXTRA_REPETITIONS_1, 1);
-		intent.putExtra(EXTRA_RHYTHM_1, new double[]{0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5});
-		intent.putExtra(EXTRA_PATTERN_LENGTH_1, 4.0);
+		intent.putExtra(EXTRA_REPETITIONS_1, 4);
+		intent.putExtra(EXTRA_RHYTHM_1, chosenRhythmPattern1.pattern);
+		intent.putExtra(EXTRA_PATTERN_LENGTH_1, chosenRhythmPattern1.patternLength);
 		startActivity(intent);
 	}
 
 	public void startDoubleLevel(View view) {
 		Intent intent = new Intent(this, DoubleLevel.class);
-		intent.putExtra(EXTRA_TEMPO, 90.0);
+		intent.putExtra(EXTRA_TEMPO, 60.0);
 		intent.putExtra(EXTRA_METRE, 4);
-		intent.putExtra(EXTRA_REPETITIONS_1, 1);
-		intent.putExtra(EXTRA_REPETITIONS_2, 1);
-		intent.putExtra(EXTRA_RHYTHM_1, new double[]{0.0, 0.5, 1.0, 2.0, 3.0, 3.25, 3.5, 3.75});
-		intent.putExtra(EXTRA_RHYTHM_2, new double[]{0.0, 1.0, 4.0/3, 3.5});
-		intent.putExtra(EXTRA_PATTERN_LENGTH_1, 4.0);
-		intent.putExtra(EXTRA_PATTERN_LENGTH_2, 4.0);
+		intent.putExtra(EXTRA_REPETITIONS_1, 4);
+		intent.putExtra(EXTRA_REPETITIONS_2, 4);
+		intent.putExtra(EXTRA_RHYTHM_1, chosenRhythmPattern1.pattern);
+		intent.putExtra(EXTRA_RHYTHM_2, chosenRhythmPattern2.pattern);
+		intent.putExtra(EXTRA_PATTERN_LENGTH_1, chosenRhythmPattern1.patternLength);
+		intent.putExtra(EXTRA_PATTERN_LENGTH_2, chosenRhythmPattern2.patternLength);
 
 		startActivity(intent);
 	}
+
+	private void initializeRhythmPatternList() {
+		rhythmPatternList.add(new RhythmPattern("Eight notes", R.drawable.papryka, 0.5, new double[]{0.0}));
+		rhythmPatternList.add(new RhythmPattern("Quarter notes", R.drawable.papryka, 1.0, new double[]{0.0}));
+	}
+
+	private ImageArrayAdapter initializeImageArrayAdapter() {
+		String[] listItems = new String[rhythmPatternList.size()];
+		int[] listImages = new int[rhythmPatternList.size()];
+		for (int i = 0; i < rhythmPatternList.size(); i++) {
+			listItems[i] = rhythmPatternList.get(i).name;
+			listImages[i] = rhythmPatternList.get(i).image;
+		}
+		return new ImageArrayAdapter(this, listItems, listImages);
+	}
+
 }
