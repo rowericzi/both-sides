e7d7e3df120666f72efc7e6263d45b03e9d875b3
Ryszard Jezierski <jezierski.ryszard@gmail.com>
2020-12-31 21:26:42 +0100

    Added README.md and license.

---
+++ README.md
@@ -0,0 +1,8 @@
+## both][sides
+A rhythm game for Android devices intended for practicing common rhythm figures using either one hand and single "rhythm track", or both hands (hence the name). Features include:
+- choosing rhythm pattern for each hand
+- choosing tempo, metre and number of measures
+- built-in metronome
+
+## License
+This program is licensed under GPL version 3.0 or later.
--- app/src/main/java/com/example/bothsides/GameManager.java
+++ app/src/main/java/com/example/bothsides/GameManager.java
@@ -1,3 +1,21 @@
+/*
+    Copyright 2020 Ryszard Jezierski
+
+    This file is part of both][sides.
+
+    both][sides is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    both][sides is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with both][sides.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.example.bothsides;
 
 import android.app.Activity;
